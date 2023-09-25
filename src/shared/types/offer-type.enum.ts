8a0273b4768da09ae448695ac1ca3a423b572d81
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-09-25 20:47:18 +0300

    feat(types): added project types

---
+++ src/shared/types/offer-type.enum.ts
@@ -0,0 +1,16 @@
+export enum OfferType {
+  Apartment = 'apartment',
+  House = 'house',
+  Room = 'room',
+  Hotel = 'hotel',
+}
+
+export enum OfferService {
+  AirConditioning = 'Air conditioning',
+  BabySeat = 'Baby seat',
+  Breakfast = 'Breakfast',
+  Fridge = 'Fridge',
+  LaptopFriendlyWorkspace = 'Laptop friendly workspace',
+  Towels = 'Towels',
+  Washer = 'Washer',
+}
