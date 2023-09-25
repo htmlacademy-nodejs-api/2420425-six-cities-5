2e767604c46098636f6c9134ca97242a4772cbdf
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-09-25 23:54:55 +0300

    feat(chalk): added chalk module

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { Command } from './command.interface.js';
 
 export class HelpCommand implements Command {
@@ -7,14 +8,14 @@ export class HelpCommand implements Command {
 
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
-        Программа для подготовки данных для REST API сервера.
-        Пример:
-            cli.js --<command> [--arguments]
+        ${chalk.blue('Программа для подготовки данных для REST API сервера')}.
+        ${chalk.bgGreen('Пример:')}
+        ${chalk.bgBlueBright('cli.js --<command> [--arguments]')}
         Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+            ${chalk.magenta('--version:')}                   # выводит номер версии
+            ${chalk.magenta('--help:')}                      # печатает этот текст
+            ${chalk.magenta('--import <path>:')}             # импортирует данные из TSV
+            ${chalk.magenta('--generate <n> <path> <url>')}  # генерирует произвольное количество тестовых данных
     `);
   }
 }
