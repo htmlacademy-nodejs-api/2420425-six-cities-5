2e65a58daf5234a756e1f162f0600e760c11cf48
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-10-08 16:29:39 +0300

    feat(config): added config to rest-application

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,11 +1,15 @@
+import { Config } from '../shared/libs/config/config.interface.js';
+import { RestSchema } from '../shared/libs/config/rest.schema.js';
 import { Logger } from '../shared/libs/logger/index.js';
 
 export class RestApplication {
   constructor(
-    private readonly logger: Logger
+    private readonly logger: Logger,
+    private readonly config: Config<RestSchema>,
   ) { }
 
   public async init() {
     this.logger.info('Application init');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
