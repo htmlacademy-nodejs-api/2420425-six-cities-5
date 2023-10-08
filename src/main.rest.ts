bdbf318cab3409b721c31f0adba969fe2ac8ab53
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-10-08 17:27:11 +0300

    feat(DI): refactor init application

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { Container } from 'inversify';
 import { RestApplication } from './rest/rest.application.js';
 import { RestConfig } from './shared/libs/config/rest.config.js';
@@ -7,9 +8,9 @@ import { Config, RestSchema } from './shared/libs/config/index.js';
 
 async function bootstrap() {
   const container = new Container();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication);
-  container.bind<Logger>(Component.Logger).to(PinoLogger);
-  container.bind<Config<RestSchema>>(Component.RestApplication).to(RestConfig);
+  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
+  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
+  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
