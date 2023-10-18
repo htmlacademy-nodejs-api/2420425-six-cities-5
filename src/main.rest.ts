4984356443803ea6eee7ef17f88c298306e5db58
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-10-19 01:07:52 +0300

    feat(rest-application): added common rest container

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,20 +1,13 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import { RestApplication } from './rest/rest.application.js';
-import { RestConfig } from './shared/libs/config/rest.config.js';
 import { Component } from './shared/types/component.enum.js';
-import { Logger, PinoLogger } from './shared/libs/logger/index.js';
-import { Config, RestSchema } from './shared/libs/config/index.js';
-import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
+import { createRestApplicationContainer } from './rest/rest.container.js';
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
-  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
+  const appContainer = Container.merge(createRestApplicationContainer());
 
-  const application = container.get<RestApplication>(Component.RestApplication);
+  const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
 
