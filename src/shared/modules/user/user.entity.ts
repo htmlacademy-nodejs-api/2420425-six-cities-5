0dec269910155ff40d7b4ed00d99cbdd172a37bd
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-10-18 21:34:18 +0300

    feat(user-model): update entity

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { User, UserType } from '../../types/index.js';
 
 export class UserEntity implements User {
+  @prop({ required: true, minlength: 1, maxlength: 15 })
   public name: string;
+
+  @prop({ unique: true, required: true })
   public email: string;
+
+  @prop({ required: true, minlength: 6, maxlength: 12 })
   public password: string;
+
+  @prop({ required: true })
   public type: UserType;
+
+  @prop({ required: false, default: 'default.jpg' })
   public avatarPath: string;
 }
+
+export const UserModel = getModelForClass(UserEntity);
