ca3d8dbd3ce102943166d21ed7bfdd937c49f3ac
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-10-19 19:05:12 +0300

    feat(user-model): update userService

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -11,7 +11,7 @@ export class DefaultUserService implements UserService {
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.UserModel) private readonly userModel: types.ModelType<UserEntity>
-  ){}
+  ) { }
 
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
@@ -21,5 +21,19 @@ export class DefaultUserService implements UserService {
     this.logger.info(`New user created: ${user.email}`);
 
     return result;
+  }
+
+  public async findByEmail(email: string): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findOne({ email });
+  }
+
+  public async findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
+    const existedUser = await this.findByEmail(dto.email);
+
+    if (existedUser) {
+      return existedUser;
+    }
+
+    return this.create(dto, salt);
   }
 }
