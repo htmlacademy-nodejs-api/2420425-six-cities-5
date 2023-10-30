024563c0e94d059c02c86dd856d7829f2481b1a8
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-10-30 23:07:37 +0300

    feat(specification): added city spec

--- src/shared/types/city.type.ts
+++ src/shared/types/city.type.ts
@@ -1,4 +1,11 @@
+import { CityName } from './city-name.enum.js';
+
 export type Coords = {
   latitude: string;
   longitude: string;
 }
+
+export type City = {
+  id: string;
+  name: CityName;
+} & Coords;
