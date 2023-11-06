8d4cc86903ba8638998ecd8e790ee5a92df39686
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-11-06 21:59:12 +0300

    feat(amenity): added amenity RDO

--- src/shared/modules/amenity/amenity.controller.ts
+++ src/shared/modules/amenity/amenity.controller.ts
@@ -2,8 +2,10 @@ import { inject, injectable } from 'inversify';
 import { Request, Response } from 'express';
 import { BaseController, HttpMethod } from '../../libs/rest/index.js';
 import { Logger } from '../../libs/logger/index.js';
+import { fillDTO } from '../../helpers/common.js';
 import { Component } from '../../types/index.js';
 import { AmenityService } from './amenity-service.interface.js';
+import { AmenityRdo } from './rdo/amenity.rdo.js';
 
 @injectable()
 export class AmenityController extends BaseController {
@@ -21,7 +23,8 @@ export class AmenityController extends BaseController {
 
   public async index(_req: Request, res: Response): Promise<void> {
     const amenities = await this.amenityService.find();
-    this.ok(res, amenities);
+    const responseData = fillDTO(AmenityRdo, amenities);
+    this.ok(res, responseData);
   }
 
   public create(_req: Request, _res: Response): void {
