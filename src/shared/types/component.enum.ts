1d428e82c852afe43af2dadbc5f17e8208d06542
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-11-06 22:29:38 +0300

    feat(rest): added AppExceptionFilter

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -12,4 +12,5 @@ export enum Component {
   AmenityController = 'AmenityController',
   CommentService = 'CommentService',
   CommentModel = 'CommentModel',
+  ExceptionFilter = 'ExceptionFilter',
 }
