66514eb54220b38cd1c7e37b44a05b901006261e
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-11-10 22:37:53 +0300

    feat(offer): added document-exists-middleware to offer-controller

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -1,9 +1,10 @@
 import { DocumentType } from '@typegoose/typegoose';
+import { DocumentExists } from '../../types/index.js';
 import { OfferEntity } from './offer.entity.js';
 import { CreateOfferDto } from './dto/create-offer.dto.js';
 import { UpdateOfferDto } from './dto/update-offer.dto.js';
 
-export interface OfferService {
+export interface OfferService extends DocumentExists {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
   find(): Promise<DocumentType<OfferEntity>[]>;
