619eb3e49b10c0770d637360531fdd42c648590b
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-11-11 20:27:27 +0300

    feat(auth): connected to rest application

--- src/shared/modules/auth/index.ts
+++ src/shared/modules/auth/index.ts
@@ -1,5 +1,4 @@
-export * from './auth.constant.js';
 export * from './auth-service.interface.js';
 export * from './default-auth.service.js';
 export * from './types/index.js';
-export * from './auth.exception-filter.js';
+export * from './auth.container.js';
