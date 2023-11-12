0ee7e7548e2383152b82dc9cd7230aa0ea7f329e
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-11-12 18:39:20 +0300

    feat(cli): update import command

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,18 +1,41 @@
 import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
-import { createOffer, getErrorMessage, getMongoURI } from '../../shared/helpers/index.js';
-import { DefaultUserService, UserModel, UserService } from '../../shared/modules/user/index.js';
-import { DatabaseClient, MongoDatabaseClient } from '../../shared/libs/database-client/index.js';
+import {
+  createOffer,
+  getErrorMessage,
+  getMongoURI,
+} from '../../shared/helpers/index.js';
+import {
+  DefaultUserService,
+  UserModel,
+  UserService,
+} from '../../shared/modules/user/index.js';
+import {
+  DatabaseClient,
+  MongoDatabaseClient,
+} from '../../shared/libs/database-client/index.js';
 import { ConsoleLogger, Logger } from '../../shared/libs/logger/index.js';
-import { DefaultOfferService, OfferModel, OfferService } from '../../shared/modules/offer/index.js';
+import {
+  DefaultOfferService,
+  OfferModel,
+  OfferService,
+} from '../../shared/modules/offer/index.js';
 import { DEFAULT_DB_PORT, DEFAULT_USER_PASSWORD } from './command.constant.js';
 import { Command } from './command.interface.js';
 import { Offer } from '../../shared/types/index.js';
-import { AmenityService } from '../../shared/modules/amenity/amenity-service.interface.js';
-import { DefaultAmenityService } from '../../shared/modules/amenity/default-amenity.service.js';
-import { AmenityModel } from '../../shared/modules/amenity/amenity.entity.js';
+import {
+  AmenityModel,
+  DefaultAmenityService,
+  AmenityService,
+} from '../../shared/modules/amenity/index.js';
+import {
+  CityModel,
+  CityService,
+  DefaultCityService,
+} from '../../shared/modules/city/index.js';
 
 export class ImportCommand implements Command {
   private amenityService: AmenityService;
+  private cityService: CityService;
   private userService: UserService;
   private offerService: OfferService;
   private databaseClient: DatabaseClient;
@@ -25,7 +48,12 @@ export class ImportCommand implements Command {
 
     this.logger = new ConsoleLogger();
     this.amenityService = new DefaultAmenityService(this.logger, AmenityModel);
-    this.offerService = new DefaultOfferService(this.logger, OfferModel, AmenityModel);
+    this.cityService = new DefaultCityService(this.logger, CityModel);
+    this.offerService = new DefaultOfferService(
+      this.logger,
+      OfferModel,
+      AmenityModel
+    );
     this.userService = new DefaultUserService(this.logger, UserModel);
     this.databaseClient = new MongoDatabaseClient(this.logger);
   }
@@ -34,7 +62,10 @@ export class ImportCommand implements Command {
     return '--import';
   }
 
-  private async onImportedLine(line: string, resolve: () => void): Promise<void> {
+  private async onImportedLine(
+    line: string,
+    resolve: () => void
+  ): Promise<void> {
     const offer = createOffer(line);
     await this.saveOffer(offer);
 
@@ -46,20 +77,29 @@ export class ImportCommand implements Command {
   }
 
   private async saveOffer(offer: Offer) {
-    const offerUser = await this.userService.findOrCreate({
-      ...offer.user, password: DEFAULT_USER_PASSWORD
-    }, this.salt);
+    const offerUser = await this.userService.findOrCreate(
+      {
+        ...offer.user,
+        password: DEFAULT_USER_PASSWORD,
+      },
+      this.salt
+    );
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { user, amenities, ...rest } = offer;
+    const { user, amenities, city, coords, ...rest } = offer;
 
-    const offerAmenities = await Promise.all(offer.amenities.map((amenity) => (
-      this.amenityService.findOrCreate({ name: amenity })
-    )));
+    const offerAmenities = await Promise.all(
+      offer.amenities.map((amenity) =>
+        this.amenityService.findOrCreate({ name: amenity })
+      )
+    );
+
+    const offerCity = await this.cityService.findOrCreate({ name: city, ...coords });
 
     await this.offerService.create({
       ...rest,
       userId: offerUser.id,
       amenities: offerAmenities.map((amenity) => amenity.id),
+      city: offerCity.id,
     });
   }
 
@@ -74,7 +114,7 @@ export class ImportCommand implements Command {
     password: string,
     host: string,
     dbname: string,
-    salt: string,
+    salt: string
   ): Promise<void> {
     const uri = getMongoURI(login, password, host, DEFAULT_DB_PORT, dbname);
     const fileReader = new TSVFileReader(filename.trim());
