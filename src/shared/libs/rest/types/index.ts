dac86ddc854f94fedbfef4c5a9c4b868d64880ee
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-11-15 22:22:32 +0300

    feat(helpers): added reduceValidationErrors

--- src/shared/libs/rest/types/index.ts
+++ src/shared/libs/rest/types/index.ts
@@ -4,3 +4,4 @@ export * from './request-query.type.js';
 export * from './request.params.type.js';
 export * from './request.type.js';
 export * from './route.interface.js';
+export * from './validation-error-field.type.js';
