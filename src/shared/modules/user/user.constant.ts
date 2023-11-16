9287fda62f0eef5023e1c6fc0237201c3bc44d6b
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-11-16 18:28:09 +0300

    feat(files): added static folder

--- src/shared/modules/user/user.constant.ts
+++ src/shared/modules/user/user.constant.ts
@@ -2,4 +2,4 @@ export const MIN_USER_NAME_LENGTH = 1;
 export const MAX_USER_NAME_LENGTH = 15;
 export const MIN_USER_PASSPORT_LENGTH = 6;
 export const MAX_USER_PASSPORT_LENGTH = 12;
-export const DEFAULT_AVATAR_FILE_NAME = 'default-avatar.jpg';
+export const DEFAULT_AVATAR_FILE_NAME = 'default-avatar.png';
