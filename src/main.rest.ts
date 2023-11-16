6a5d4243462b8ffe5e0d71868d8076a738fe72fa
Пузидаров Никита <nikita.puzidarov@gazprombank.ru>
2023-11-16 18:33:35 +0300

    refactor(rest): moved paths to constants

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,8 +1,7 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { RestApplication } from './rest/rest.application.js';
+import { createRestApplicationContainer, RestApplication } from './rest/index.js';
 import { Component } from './shared/types/component.enum.js';
-import { createRestApplicationContainer } from './rest/rest.container.js';
 import { createUserContainer } from './shared/modules/user/index.js';
 import { createOfferContainer } from './shared/modules/offer/index.js';
 import { createAmenityContainer } from './shared/modules/amenity/index.js';
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -6,6 +6,7 @@ import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { Component } from '../shared/types/index.js';
 import { getFullServerPath, getMongoURI } from '../shared/helpers/index.js';
 import { Controller, ExceptionFilter, ParseTokenMiddleware } from '../shared/libs/rest/index.js';
+import { STATIC_FILES_ROUTE, STATIC_UPLOAD_ROUTE } from './rest.constant.js';
 
 @injectable()
 export class RestApplication {
@@ -57,11 +58,11 @@ export class RestApplication {
     const authenticateMiddleware = new ParseTokenMiddleware(this.config.get('JWT_SECRET'));
     this.server.use(express.json());
     this.server.use(
-      '/upload',
+      STATIC_UPLOAD_ROUTE,
       express.static(this.config.get('UPLOAD_DIRECTORY'))
     );
     this.server.use(
-      '/static',
+      STATIC_FILES_ROUTE,
       express.static(this.config.get('STATIC_DIRECTORY_PATH'))
     );
     this.server.use(authenticateMiddleware.execute.bind(authenticateMiddleware));
